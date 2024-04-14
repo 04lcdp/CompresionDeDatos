c438c49d1e48a5af762af0d6c45bc632d5da8686
04lcdp <luisdonayrep.2004@gmail.com>
2024-04-14 13:48:39 -0500

    tercer commit

--- CompresionDeDatos.py
+++ CompresionDeDatos.py
@@ -8,8 +8,6 @@ from tkinter import filedialog
 def elegir_y_leer_archivo():
     root = tk.Tk()
     root.withdraw()
-
-    # Mostramos el cuadro de diálogo para seleccionar el archivo
     ruta_archivo = filedialog.askopenfilename(
         title="Selecciona un archivo",
         filetypes=(("Archivos de texto", "*.txt"), ("Todos los archivos", "*.*"))
@@ -18,6 +16,7 @@ def elegir_y_leer_archivo():
         try:
             with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
                 contenido = archivo.read()
+                print(contenido)
             return contenido
         except Exception as e:
             print(f"Error al leer el archivo: {e}")
@@ -29,18 +28,17 @@ def elegir_y_leer_archivo():
 def Compresion(mensaje):
     return mensaje
 
-def main():
-    opcion = input ("COMPRESION DE DATOS\n1. Compresion de Datos\n2.Descompresion de Datos\n\nElige tu opcion -> ")
-    if opcion == '1':
-        contenido_archivo = elegir_y_leer_archivo()
-        if contenido_archivo:
-            print("Contenido del archivo:")
-            print(contenido_archivo)
-        else:
-            print("No se pudo leer ningún contenido.")
 
-if __name__ == "__main__":
-    main()
+opcion = input ("COMPRESION DE DATOS\n1. Compresion de Datos\n2.Descompresion de Datos\n\nElige tu opcion -> ")
+if opcion == '1':
+    contenido = elegir_y_leer_archivo()
+#if contenido_archivo:
+    #print("Contenido del archivo:")
+    #print(contenido_archivo)
+#else:
+    #print("No se pudo leer ningún contenido.")
+
+
 
 
 #mensaje = "hola"
